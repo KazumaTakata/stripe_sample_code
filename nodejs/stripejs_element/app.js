f4842dae85392cc1552ab10a45375061072d7375
Kazuma Takata <neilbuzztakata@gmail.com>
2018-08-19 17:25:41 +0900

    add stripe element

---
+++ nodejs/stripejs_element/app.js
@@ -0,0 +1,37 @@
+// const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keyPublishable = "pk_test_g7Ms2DsBHY35cWjejL9NK9Sh";
+// const keySecret = process.env.SECRET_KEY;
+const keySecret = "sk_test_tuIahCxwHn795ta50bFl70ik";
+
+const express = require("express");
+const app = express();
+const stripe = require("stripe")(keySecret);
+const bodyParser = require("body-parser");
+
+app.set("view engine", "pug");
+app.use(require("body-parser").urlencoded({ extended: false }));
+app.use(express.static("statics"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.render("stripe_element.pug"));
+
+app.post("/charge", (req, res) => {
+  let amount = 200;
+
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken,
+    })
+    .then(customer =>
+      stripe.charges.create({
+        amount,
+        description: "Sample Charge",
+        currency: "usd",
+        customer: customer.id,
+      })
+    )
+    .then(charge => res.render("charge.pug"));
+});
+
+app.listen(4570);
